3dff53380fb2d43cb97012aa5fe60864555d5a89
a9d94t9h9 <axeldavidled999@gmail.com>
2022-11-14 18:32:59 -0300

    Ruta y controlador para ver hoteles y filtrar

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,7 +1,8 @@
 let router = require('express').Router()
 
-let { create } = require('../controllers/hotel')
+let { create, read } = require('../controllers/hotel')
 
 router.route('/hotels').post(create)
+router.route('/hotels').get(read)
 
 module.exports = router;
