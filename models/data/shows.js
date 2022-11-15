9ef5e31f62375d7adae1eab280f14c750bebd0de
a9d94t9h9 <axeldavidled999@gmail.com>
2022-11-15 03:21:46 -0300

    Modelo show creado

---
+++ models/data/shows.js
@@ -0,0 +1,128 @@
+let shows = [
+    {
+        "hotelId": "636ddfc72cfcb8c610a9f783",
+        "name": "TINI",
+        "photo": "https://pbs.twimg.com/profile_images/1524785602813521925/ao7IeqrI_400x400.jpg",
+        "price": 5500,
+        "date": new Date("2022,12,12"),
+        "userId": "636d864fec3e352a19f44e9f",
+        "description": "Tini arrives in Rosario as part of a national tour with a wave of hits and an incredible staging."
+    },
+    {
+        "hotelId": "636ddfc72cfcb8c610a9f783",
+        "name": "Malvinas. Canto al sentimiento de un pueblo",
+        "photo": "https://www.teatroenrosario.com//imagenes/obras/132e7042c0ec40a33d9e0c96ff699858.jpg",
+        "price": 50,
+        "date": new Date(2022, 12, 12),
+        "userId": "636d864fec3e352a19f44e9f",
+        "description": "It is a theatrical-musical show, a tribute that has a pedagogical and national character. It is constituted as a living document of part of the history of our country."
+    },
+    {
+        "hotelId": "636ddfc72cfcb8c610a9f784",
+        "name": "Michael Bublé",
+        "photo": "https://cdn.wegow.com/media/artists/michael-buble/michael-buble-1495709446.78.2560x1440.png",
+        "price": 200,
+        "date": new Date("2022,12,12"),
+        "userId": "636d864fec3e352a19f44ea0",
+        "description": "The artist's presentation will take place within the framework of his world tour 'An evening with Michael Bublé', which had to be suspended due to the pandemic."
+    },
+    {
+        "hotelId": "636ddfc72cfcb8c610a9f784",
+        "name": "El mar de noche",
+        "photo": "https://img.alternativateatral.com/scripts/es/fotos/obras/resumen/000161554.jpg",
+        "price": 70,
+        "date": new Date("2022,12,12"),
+        "userId": "636d864fec3e352a19f44ea0",
+        "description": "Luis Machín arrives in Rosario with the solo show El mar de noche, written by Santiago Loza and directed by Guillermo Cacace."
+    },
+    {
+        "hotelId": "636ddfc72cfcb8c610a9f785",
+        "name": "Michel telo",
+        "photo": "https://s2.abcstatics.com/Media/201202/09/michel-telo--478x270.jpg",
+        "price": 200,
+        "date": new Date("2022,12,12"),
+        "userId": "636d864fec3e352a19f44e9e",
+        "description": "Lorem ipsum dolor sit amet"
+    },
+    {
+        "hotelId": "636ddfc72cfcb8c610a9f785",
+        "name": "4 AMIGOS",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS95Fu9i33ZwraM8IXZTJygjBomxXtIRV6v6t3TXWrVTvEYIjL3vYpVc72UtKk2xmWziGM&usqp=CAU",
+        "price": 20,
+        "date": new Date("2022,12,12"),
+        "userId": "636d864fec3e352a19f44e9e",
+        "description": "Campo Grande (MS) - Longtime friends, the comedians Thiago Ventura, Dihh Lopes, Marcio Donato and Afonso Padilha will perform on July 7th (Sunday), in Campo Grande, at the Rubens Gil de Camillo Convention Center, with the comedy 4 friends."
+    },
+    {
+        "hotelId": "636ddfc72cfcb8c610a9f786",
+        "name": "Eros Ramazzotti",
+        "photo": "https://eventicket.s3-sa-east-1.amazonaws.com/imgs/23823-img-destaque.jpg",
+        "price": 120,
+        "date": new Date("2022,12,12"),
+        "userId": "636d864fec3e352a19f44e9d",
+        "description": "Eros Ramazzotti is one of Italy's most globally recognised musicians due to both his success in his homeland but also his unrelenting touring schedule and dedication to reaching his global fans."
+    },
+    {
+        "hotelId": "636ddfc72cfcb8c610a9f786",
+        "name": "Chico Buarque - Turnê Que tal um samba",
+        "photo": "https://turismoemfoco.com.br/v1/wp-content/uploads/2022/06/Chico-Buarque-por-Francisco-Proner-.jpg",
+        "price": 15,
+        "date": new Date("2022,12,12"),
+        "userId": "636e8c06ce259ab0ebdb9813",
+        "description": "Four years after his last show, Chico Buarque is getting ready to return to the stage."
+    },
+    {
+        "hotelId": "636ddfc72cfcb8c610a9f787",
+        "name": "Jerry Rivera",
+        "photo": "http://images.sk-static.com/images/media/img/col6/20100309-172434-917668.png",
+        "price": 45,
+        "date": new Date("2022,12,12"),
+        "userId": "636e8c06ce259ab0ebdb9813",
+        "description": "Jerry Rivera is an artist from Puerto Rico. He was born in 1973. His style is characterized as Latina and Latin."
+    },
+    {
+        "hotelId": "636ddfc72cfcb8c610a9f787",
+        "name": "Permitido Equivocarse",
+        "photo": "http://static-1.ivoox.com/audios/4/1/7/0/4170c8efc9dc5103cda024541789d871_LG.jpg",
+        "price": 20,
+        "date": new Date("2022,12,12"),
+        "userId": "636d864fec3e352a19f44e9d",
+        "description": "a show between two women from two brother countries, a meeting without great expectations, remember anecdotes, give their point of view on infinite situations in life and see how the greatest tragedy can become, with time and wounds healed , in a great celebration."
+    },
+    {
+        "hotelId": "636ddfc72cfcb8c610a9f788",
+        "name": "Wisin y Yandel: La Ultima Mision",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/4/42/Wisin_yandel_La_UltimaMisionTour.JPEG",
+        "price": 170,
+        "date": new Date("2022,12,12"),
+        "userId": "636d864fec3e352a19f44ea0",
+        "description": "La Última Misión is the tenth and final studio album by Puerto Rican reggaeton duo Wisin & Yandel, released on September 30, 2022."
+    },
+    {
+        "hotelId": "636ddfc72cfcb8c610a9f788",
+        "name": "Besame Mucho Festival",
+        "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV7s3Ua7OVje0oTFrtB4sffNAJcwlE3AcSrXfdorYHFpHNoIdI9IPC5WvZjnyQkUnmSVQ&usqp=CAU",
+        "price": 30,
+        "date": new Date("2022,12,12"),
+        "userId": "636e8c06ce259ab0ebdb9813",
+        "description": "FESTIVAL TO FEATURE AN ICONIC MULTIGENERATIONAL LINEUP OF LATIN ARTISTS ACROSS ROCKERO, LAS CLASICAS, AND TE GUSTA EL POP! STAGES"
+    }
+]
+
+require('dotenv').config()
+require('../../config/database/database')
+const Show = require('../Show')
+
+shows.forEach(show => {
+    Show.create({
+        name : show.name,
+        description: show.description,
+        photo: show.photo,
+        price: show.price,
+        date: show.date,
+        hotelId: show.hotelId,
+        userId: show.userId
+    })
+})
+
+
