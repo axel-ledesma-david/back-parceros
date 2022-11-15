eff588819b7d6513abb2024492e8ab05c4b7749b
Alejandro Velez <alejovelez@nebular.co>
2022-11-15 00:04:49 -0500

    Se agrega get para ver los itinerarios por ciudad

--- routes/itinerarycity.js
+++ routes/itinerarycity.js
@@ -2,12 +2,12 @@
 let router =require('express').Router()
 
 
-const { Create,update,destroy } = require('../controllers/itinerarycity');
+const { Create,update,destroy,readitinerarycity } = require('../controllers/itinerarycity');
 
 
 router.route('/itineraries').post(Create)
 router.route('/itineraries/:id').put(update)
 router.route('/itineraries/:id').delete(destroy)
-
+router.route('/itineraries').get(readitinerarycity)
 
 module.exports = router;
