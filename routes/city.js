b915968e5a1043083451530c8c2d5d8a1b95e399
Alejandro Velez <alejovelez@nebular.co>
2022-11-14 23:48:24 -0500

    creacion rutas y controladores para ver las ciudades

--- routes/city.js
+++ routes/city.js
@@ -2,9 +2,11 @@
 let router =require('express').Router()
 
 
-const { Create,read,update,destroy } = require('../controllers/city');
+const { Create,read,readcitybyid,update,destroy } = require('../controllers/city');
 
 router.route('/cities').get(read)
+router.route('/cities/:id').get(readcitybyid)
+
 router.route('/cities').post(Create)
 router.route('/cities/:id').put(update)
 router.route('/cities/:id').delete(destroy)
