ac622baf4fcb1ad3dc8f6d43e4daa37d8ad5fd3c
a9d94t9h9 <axeldavidled999@gmail.com>
2022-11-15 10:35:39 -0300

    ruta y controlador para editar hoteles

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,9 +1,10 @@
 let router = require('express').Router()
 
-let { create, read, readHotelDetail } = require('../controllers/hotel')
+let { create, read, readHotelDetail, update } = require('../controllers/hotel')
 
 router.route('/hotels').post(create)
 router.route('/hotels').get(read)
 router.route('/hotels/:id').get(readHotelDetail)
+router.route('/hotels/:id').put(update)
 
 module.exports = router;
