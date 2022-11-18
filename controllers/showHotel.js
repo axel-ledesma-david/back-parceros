0aeefe75588086eabd98c98cffe28fe4cc8c3ab6
a9d94t9h9 <axeldavidled999@gmail.com>
2022-11-17 22:54:57 -0300

    Ruta y controlador para editar un show con metodo patch

--- controllers/showHotel.js
+++ controllers/showHotel.js
@@ -35,6 +35,32 @@ const controller = {
                 message: err.message
             })
         }
+    },
+    update: async (req, res) => {
+        let {id} = req.params
+
+        try {
+            let show = await Show.findOneAndUpdate({_id : id}, req.body, { new: true })
+
+            if (show) {
+                res.status(200).json({
+                    res: show._id,
+                    success: true,
+                    message: "The show has been modified successfully"
+                })
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: "The show is not found"
+                })
+            }
+
+        } catch (err) {
+            res.status(400).json({
+                success: false,
+                message: err.message
+            })
+        }
     }
 }
  
