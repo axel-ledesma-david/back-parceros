f435688a9c4939b4304beef50710fe2ae47146ce
a9d94t9h9 <axeldavidled999@gmail.com>
2022-11-22 01:25:07 -0300

    testing de envio de datos de hotel y validaciones con joi

--- routes/hotel.js
+++ routes/hotel.js
@@ -1,8 +1,10 @@
 let router = require('express').Router()
+const schema = require('../schema/hotel')
+const validator = require('../middlewares/validatorHotel')
 
 let { create, read, readHotelDetail, update, destroy } = require('../controllers/hotel')
 
-router.route('/hotels').post(create)
+router.route('/hotels').post( validator(schema), create)
 router.route('/hotels').get(read)
 router.route('/hotels/:id').get(readHotelDetail)
 router.route('/hotels/:id').patch(update)
