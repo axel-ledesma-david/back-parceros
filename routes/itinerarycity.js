bfcc83ee67d94b310c613af749f061f148482144
Alejandro Velez <alejovelez@nebular.co>
2022-11-24 11:14:29 -0500

    editar y eliminar city-itinerary

--- routes/itinerarycity.js
+++ routes/itinerarycity.js
@@ -2,12 +2,17 @@
 let router =require('express').Router()
 
 
-const { Create,update,destroy,readitinerarycity } = require('../controllers/itinerarycity');
+const { Create,update,destroy,readitinerarycity,readitinerarybyid } = require('../controllers/itinerarycity');
 
 
 router.route('/itineraries').post(Create)
 router.route('/itineraries/:id').put(update)
+// itineraries/12354
+router.route('/itineraries/:id').get(readitinerarybyid)
 router.route('/itineraries/:id').delete(destroy)
 router.route('/itineraries').get(readitinerarycity)
+// itineraries?userid=354
 
 module.exports = router;
+
+
