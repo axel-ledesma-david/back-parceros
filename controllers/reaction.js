96769c5dd12fc3cd81dface5c9d74ca0bca90cce
Alejandro Velez <alejovelez@nebular.co>
2022-12-01 17:12:45 -0500

    Para que los usuarios logueados utilizar estas reacciones en itinerarios

--- controllers/reaction.js
+++ controllers/reaction.js
@@ -17,36 +17,117 @@ const controller = {
       });
     }
   },
-
   update: async (req, res) => {
-    let { id } = req.params;
+    let query = {};
+
+
+    if (req.query.name) {
+      query = {
+        ...query,
+        name: req.query.name,
+      };
+    }
+
+    if (req.query.itineraryId) {
+      query = {
+        ...query,
+        itineraryId: req.query.itineraryId,
+      };
+    }
 
     try {
-      let one = await Reaction.findOneAndUpdate({ _id: id }, req.body, {
-        new: true,
+      let all = await Reaction.find(query);
+      res.status(200).json({
+        response: all,
+        success: true,
+        message: "Reaction Update",
       });
-
-      if (one) {
-        res.status(200).json({
-          id: one._id,
-          success: true,
-          message: "the reaction was update successfully",
-        });
-      } else {
-        res.status(400).json({
-          success: false,
-          message: "the reaction was not found",
-        });
-      }
     } catch (error) {
       res.status(400).json({
         success: false,
         message: error.message,
+      }); 
+    }
+},
+
+readitineraryid: async (req, res) => {
+  let query = {};
+
+  if (req.query.itineraryId) {
+    query = {
+      ...query,
+      itineraryId: req.query.itineraryId,
+    };
+  }
+
+  try {
+    let all = await Reaction.find(query);
+    res.status(200).json({
+      response: all,
+      success: true,
+      message: "Reaction were found",
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    }); 
+  }
+},
+
+readuserid: async (req, res) => {
+  let query = {};
+
+  if (req.query.userId) {
+    query = {
+      ...query,
+      userId: req.query.userId,
+    };
+  }
+
+  try {
+    let all = await Reaction.find(query);
+    res.status(200).json({
+      response: all,
+      success: true,
+      message: "Reaction were found",
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    }); 
+  }
+},
+
+destroy: async (req, res) => {
+  let { id } = req.params;
+  try {
+    let one = await Reaction.findOneAndDelete({ _id: id });
+    if (one) {
+      res.status(200).json({
+        id: one._id,
+        success: true,
+        message: "the reaction was deleted successfully",
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "the reactions was not found",
       });
     }
-  },
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+},
+
 
 }
+
+
 
  
 
--- routes/reaction.js
+++ routes/reaction.js
@@ -3,14 +3,17 @@ let router =require('express').Router()
 const schema = require('../schemas/reaction')
 const validator = require('../middleware/validator')
 
-const { create,update } = require('../controllers/reaction');
+const { create,update,readitineraryid,readuserid,destroy } = require('../controllers/reaction');
 
 
 
 
 
 router.route('/reactions').post(validator(schema),create)
-router.route('/reactions/:id').put(update)
+router.route('/reactions').put(update)
+router.route('/reactions').get(readitineraryid)
+router.route('/reactions').get(readuserid)
+router.route('/reactions/:id').delete(destroy)
 
 
 module.exports = router;
