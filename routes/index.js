854bfdd7fbfbff3691dcb8b0559c4d994230c203
a9d94t9h9 <axeldavidled999@gmail.com>
2022-12-01 01:47:57 -0300

    Rutas, controladores y modelo de Comment

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ let itinerarycity = require("./itinerarycity");
 let hotel = require("./hotel");
 let city = require("./city");
 let showHotel = require("./showHotel");
+let comment = require('./comment')
 
 router.use("/auth", user);
 router.use("/api", itinerarycity);
 router.use("/api", hotel);
 router.use("/api", city);
 router.use("/api", showHotel);
+router.use("/api", comment)
 
 module.exports = router;
